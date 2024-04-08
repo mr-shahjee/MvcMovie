c5ae71de2b8bdbc44b93ed22beb082f74d5c98a0
mr-shahjee <abdullahshahjee7@gmail.com>
2024-04-08 15:08:43 +0500

    first comit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+using DataAccessLayer;
+using Models;
+
+namespace MvcMovie.Controllers;
+
+public class HomeController : Controller
+{ 
+    private readonly EmployeesDAL empDAL;
+    public HomeController( )
+    {
+        empDAL = new EmployeesDAL();
+    }
+
+    public IActionResult Index()
+    {
+        List<Employees> emps = empDAL.getAllEmployees();
+        return View(emps);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ utilities/ConnectionString.cs
@@ -0,0 +1,6 @@
+namespace utilities{
+    public static class ConnectionString{
+        private static string cs = "SERVER:DESKTOP-7FMSL83; Database: CrudADOdb; Trusted_Connection:True;";
+    public static string dbcs {get => cs;}
+    }
+}
